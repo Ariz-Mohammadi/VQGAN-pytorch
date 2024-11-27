f2b28ab299d67daebafd66ba4823880f1498d81e
Ariz Mohammadi <161158740+Ariz-Mohammadi@users.noreply.github.com>
2024-11-27 20:58:53 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -9,7 +9,8 @@ class Encoder(nn.Module):
         attn_resolutions = [16]
         num_res_blocks = 2
         resolution = 256
-        layers = [nn.Conv2d(args.image_channels, channels[0], 3, 1, 1)]
+        #layers = [nn.Conv2d(args.image_channels, channels[0], 3, 1, 1)]injarocommentkardambadanmodifykon 
+        layers = [nn.Conv2d(1, channels[0], 3, 1, 1)]
         for i in range(len(channels)-1):
             in_channels = channels[i]
             out_channels = channels[i + 1]
